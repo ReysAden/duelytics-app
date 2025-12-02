89d28efb854ab3de6f7a939f55ff0ffe0ba1e4f8
Reys Aden <adenreys1@gmail.com>
2025-12-01 20:23:03 -0600

    Use HashRouter for Electron file:// protocol compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { supabase } from './lib/supabase'
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import toast from 'react-hot-toast'
 import { Background } from './components/Background'
